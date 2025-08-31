761d27de8ff5d96b8755a42cea15487471af5069
Jamie Smith <jamielsmith1118@gmail.com>
2025-08-31 11:43:30 -0500

    Attempted to import key from creds, but it fails

--- api_testing.py
+++ api_testing.py
@@ -1,8 +1,9 @@
 # url = "https://v1.american-football.api-sports.io/leagues"
-#
+# from creds import api_key
+
 # payload={}
 # headers = {
-#   'x-rapidapi-key': '9fe360f84b458f8f87f7dbc6119a9323',
+#   'x-rapidapi-key': 'api_key',
 #   'x-rapidapi-host': 'v1.american-football.api-sports.io'
 # }
 #
@@ -20,33 +21,38 @@
 # get("https://v1.american-football.api-sports.io/status");
 
 import http.client
-from pprint import pprint
 
-import requests
+# from pprint import pprint
+
+import pprint
 
 conn = http.client.HTTPSConnection("v1.american-football.api-sports.io")
 
+from creds import api_key
+
 headers = {
-    'x-rapidapi-host': "v1.american-football.api-sports.io",
-    'x-rapidapi-key': "9fe360f84b458f8f87f7dbc6119a9323"
-    }
+    "x-rapidapi-host": "v1.american-football.api-sports.io"
+'x-rapidapi-key':'api_key',
+"content-type": "application/json"
+}
 
 conn.request("GET", "/leagues", headers=headers)
 
 res = conn.getresponse()
 data = res.read()
 
-print(data.decode("utf-8"))
+# print(data.decode("utf-8"))
+pprint.pprint(data)
 
-
-url = "https://v1.american-football.api-sports.io/leagues"
-
-payload={}
-headers = {
-  'x-rapidapi-key': '9fe360f84b458f8f87f7dbc6119a9323',
-  'x-rapidapi-host': 'v1.american-football.api-sports.io'
-}
-
-response = requests.request("GET", url, headers=headers, data=payload)
-
-pprint(response.text)
+# url = "https://v1.american-football.api-sports.io/leagues"
+#
+# payload = {}
+# headers = {
+#     'x-rapidapi-key': 'api_key',
+#     'x-rapidapi-host': 'v1.american-football.api-sports.io'
+# }
+#
+# response = requests.request("GET", url, headers=headers, data=payload)
+#
+# # print(response.text)
+# pprint.pprint(data)
