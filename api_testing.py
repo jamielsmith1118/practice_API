d6f04961128a8d60abed7787ad2d30dcf6fb2039
Jamie Smith <jamielsmith1118@gmail.com>
2025-08-31 12:36:45 -0500

    Reached API limit for the day. Teams 1-24 have been identified.

--- api_testing.py
+++ api_testing.py
@@ -1,60 +1,78 @@
-# url = "https://v1.american-football.api-sports.io/leagues"
+# # url = "https://v1.american-football.api-sports.io/leagues"
+#
+# # api_key = '9fe360f84b458f8f87f7dbc6119a9323'
+#
+# # payload={}
+# # headers = {
+# #   'x-rapidapi-key': 'api_key',
+# #   'x-rapidapi-host': 'v1.american-football.api-sports.io'
+# # }
+# #
+# # response = requests.request("GET", url, headers=headers, data=payload)
+# #
+# # print(response.text)
+# #
+# #
+# #
+# #
+# #
+# #
+# #
+# #
+# # get("https://v1.american-football.api-sports.io/status");
+#
+# import http.client
+#
+#
+# import pprint
+#
+# conn = http.client.HTTPSConnection("v1.american-football.api-sports.io")
+#
+import pprint
 
-# api_key = '9fe360f84b458f8f87f7dbc6119a9323'
-
-# payload={}
+from creds import sports_key
 # headers = {
-#   'x-rapidapi-key': 'api_key',
-#   'x-rapidapi-host': 'v1.american-football.api-sports.io'
+#     'x-rapidapi-host': "v1.american-football.api-sports.io",
+# 'x-rapidapi-key': sports_key,
+# 'content-type': 'application/json'
+# }
+#
+# conn.request("GET", "/leagues", headers=headers)
+#
+# res = conn.getresponse()
+# data = res.read()
+#
+# # print(data.decode("utf-8"))
+# pprint.pprint(data)
+#
+# import requests
+#
+# url = "https://v1.american-football.api-sports.io/leagues"
+#
+# payload = {}
+# headers = {
+#     'x-rapidapi-key': sports_key,
+#     'x-rapidapi-host': 'v1.american-football.api-sports.io'
 # }
 #
 # response = requests.request("GET", url, headers=headers, data=payload)
 #
-# print(response.text)
-#
-#
-#
-#
-#
-#
-#
-#
-# get("https://v1.american-football.api-sports.io/status");
+# # print(response.text)
+# pprint.pprint(requests.Request)
 
 import http.client
-
-# from pprint import pprint
-
-import pprint
+import json
 
 conn = http.client.HTTPSConnection("v1.american-football.api-sports.io")
 
-from creds import sports_key
 headers = {
     'x-rapidapi-host': "v1.american-football.api-sports.io",
-'x-rapidapi-key': sports_key,
-'content-type': 'application/json'
-}
+    'x-rapidapi-key': sports_key
+    }
 
-conn.request("GET", "/leagues", headers=headers)
+conn.request("GET", "/teams?id=24", headers=headers)
 
 res = conn.getresponse()
 data = res.read()
 
-# print(data.decode("utf-8"))
-pprint.pprint(data)
-
-import requests
-
-url = "https://v1.american-football.api-sports.io/leagues"
-
-payload = {}
-headers = {
-    'x-rapidapi-key': sports_key,
-    'x-rapidapi-host': 'v1.american-football.api-sports.io'
-}
-
-response = requests.request("GET", url, headers=headers, data=payload)
-
-# print(response.text)
-pprint.pprint(requests.Request)
+pprint.pprint(json.loads(data.decode("utf-8")))
